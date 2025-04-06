6cb779fcd95c7d38f93fca6eee181c6b41ab4416
Kristin Dailey <kristin.dailey02@gmail.com>
2025-04-06 12:34:37 -0700

    Set up Stream and OpenAI

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import { StreamChat } from "stream-chat";
+import OpenAI from "openai";
 
 dotenv.config();
 
@@ -9,6 +11,69 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// Initialize Stream Client
+const chatClient = StreamChat.getInstance(
+    process.env.STREAM_API_KEY!,
+    process.env.STREAM_API_SECRET!,
+);
+
+// Initialize OpenAI 
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+// Register user with Stream Chat
+app.post("/register-user", async (req: Request, res: Response):Promise<any> => {
+    const { name, email } = req.body || {};
+
+    if (!name || !email) {
+        return res.status(400).json({error: "Name and email are required."});
+    }
+
+    try {
+        const userId = email.replace(/[^a-zA-Z0-9_-]/g, "_");
+
+        // Check if user exists 
+        const userResponse = await chatClient.queryUsers({ id: { $eq: userId }});
+
+        if (!userResponse.users.length) {
+            // Add new user to stream
+            await chatClient.upsertUser({
+                id: userId, 
+                name: name,
+                email: email, 
+                role: "user",
+            });
+        }
+
+        res.status(200).json({ userId, name, email });
+    } catch (error) {
+        res.status(500).json({error: "Internal server error."});   
+    }
+});
+
+// Send message to AI
+app.post("/chat", async (req: Request, res: Response): Promise<any> => {
+    const { message, userId } = req.body;
+
+    if (!message || !userId) {
+        return res.status(400).json({error: "Message and user are required."});
+    }
+
+    try {
+        // Verify user exists
+        const userResponse = await chatClient.queryUsers({ id: userId });
+
+        if (!userResponse.users.length) {
+            return res.status(404).json({error: "User not found. Please register first."});
+        }
+
+        res.send("Success!");
+    } catch (error) {
+        return res.status(500).json({error: "Internal server error."});
+    }
+});
 
 const PORT = process.env.PORT || 5000;
 
