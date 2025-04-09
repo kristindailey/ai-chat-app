f45f8a9f0896b2ef878abc049f7bc204215c9c6b
Kristin Dailey <kristin.dailey02@gmail.com>
2025-04-09 13:22:58 -0700

    Added header component

---
+++ ai-chat-ui/src/components/Header.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+    import robotImage from "../assets/robot.png";
+</script>
+
+<template>
+    <div class="py-4 px-6 bd-gray-800 shadow-md flex justify-between items-center">
+        <img :src="robotImage" alt="Robot image" class="w-8 h-8">
+    </div>
+</template>
